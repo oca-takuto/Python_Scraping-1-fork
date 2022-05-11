cfe30b93cd43f11c1f36f3efc215552747c05a95
Fujii Noritsugu <quercus491@gmail.com>
2022-05-12 05:57:51 +0900

    クローリングのサンプル

--- test_selenium.py
+++ test_selenium.py
@@ -2,12 +2,32 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
+import time
+from selenium.webdriver.common.by import By
+
 
 option = Options()
 option.add_argument('--headless')
 
 driver = webdriver.Chrome()
 driver.get('https://www.google.co.jp/')
+
+# 検索フィールドの取得
+query = driver.find_element(by=By.NAME, value='q')
+
+# 検索文字列を入力
+query.send_keys('クローリング')
+
+# 3秒待つ
+time.sleep(3)
+
+# 検索ボタンをクリック
+button = driver.find_element(by=By.NAME, value='btnK')
+button.click()
+
+# 3秒待つ
+time.sleep(3)
+
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
